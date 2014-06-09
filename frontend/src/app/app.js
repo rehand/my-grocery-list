88277449711cbb4f4f11a25a2ea2512d72e97223
rehand <reinhard.handler@gmail.com>
2014-06-09 12:01:39 +0200

    added HTTP interceptor to send CSRF data for all requests changed method for updates from POST to PATCH added navigateUp function to utils

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -8,8 +8,33 @@ angular.module( 'myGroceryList', [
   'ngResource'
 ])
 
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
+.factory('authHttpResponseInterceptor', function($q, $location, $log){
+    return {
+        response: function(response){
+            if (response.status === 403) {
+                $log.log("Response " + response.status);
+            }
+            return response || $q.when(response);
+        },
+        responseError: function(rejection) {
+            if (rejection.status === 403) {
+                $log.log("Response Error ", rejection);
+                $location.path('/login').search('returnTo', $location.path());
+            }
+            return $q.reject(rejection);
+        }
+    };
+})
+
+.config( function myAppConfig ($urlRouterProvider, $httpProvider) {
+  $urlRouterProvider.otherwise('/home');
+  
+  // set CSRF cookie and header name
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  
+  // HTTP interceptor to check auth failures for XHR requests
+  $httpProvider.interceptors.push('authHttpResponseInterceptor');
 })
 
 .run( function run ($rootScope, $state, $stateParams) {
@@ -26,11 +51,18 @@ angular.module( 'myGroceryList', [
 })
 
 .factory('groceryListFactory', function($resource) {
-    return $resource('/api/list/:listId', { listId:'@id' });
+    return $resource('/api/list/:listId', 
+            { listId: '@id' },
+            // use PATCH for partial updates
+            { update: { method: 'PATCH' }});
 })
 
 .factory('GroceryListEntryFactory', function($resource) {
-    return $resource('/api/entry/:entryId', { entryId:'@id' });
+    return $resource('/api/entry/:entryId',
+            { entryId: '@id' },
+            // use PATCH for partial updates
+            { update: { method: 'PATCH' }}
+    );
 })
 
 .factory('utils', function() {
@@ -43,6 +75,9 @@ angular.module( 'myGroceryList', [
         }
       }
       return null;
+    },
+    navigateUp: function navigateUp(state, stateParams) {
+        state.go('^', stateParams);
     }
   };
 })
