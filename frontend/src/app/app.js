26fa1d681a22f00839660aa07b18ec9b2325469b
rehand <reinhard.handler@gmail.com>
2014-06-10 11:08:26 +0200

    bugfix if state doesn't have pageTitle, etc. defined

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -44,7 +44,7 @@ angular.module( 'myGroceryList', [
 
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
+    if (angular.isDefined(toState) && angular.isDefined(toState.data) && angular.isDefined(toState.data.pageTitle)) {
       $scope.pageTitle = toState.data.pageTitle + ' | myGroceryList' ;
     }
   });
